536307b1c9523059b87fbb0ef75197b11a4ce829
박지윤 <tkrhkfld123@naver.com>
2024-01-21 01:22:14 +0900

    문자 반복 출력하기

--- 문자반복출력하기.js
+++ 문자반복출력하기.js
@@ -1,7 +1,7 @@
 function solution(my_string, n) {
   let result = '';
-  for (let i = 0; i < myString.length; i++) {
-      result += myString[i].repeat(n);
+  for (let i = 0; i < my_string.length; i++) {
+      result += my_string[i].repeat(n);
   }
   return result;
 }
