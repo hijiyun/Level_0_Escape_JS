a4d26ca1e3531df2f388e1f55176be4c8a181243
jinah-dev <155082326+jinah-dev@users.noreply.github.com>
2024-01-22 15:53:42 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    if (num1 === num2){
+        return 1;
+    } else return -1;
+}
+
+// function solution(num1, num2) {
+//     const result = num1 === num2 ? 1 : -1;
+//     return result
+// }
+
+// const solution = (num1, num2) => num1 === num2 ? 1 : -1; 
