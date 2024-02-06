0681250d3ba1bffa344190e76e1d520a9294bfb2
jinah-dev <155082326+jinah-dev@users.noreply.github.com>
2024-02-06 20:37:12 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    return my_string.split('').reverse().join('');
+}
+
+/*
+function solution(my_string) {
+  let answer = '';
+  answer = my_string.split('').reverse().join('');
+  return answer;
+}
+*/
