a4c42ec33fe591ebccf84d1b23594a038665f5d9
박지윤 <tkrhkfld123@naver.com>
2024-02-11 16:24:02 +0900

    4주차

---
+++ 분수의 덧셈.js
@@ -0,0 +1,14 @@
+function fractionalAddition(num, den, max) {
+  for (let i = 1; i <= num; i++) {
+    num % i === 0 && den % i === 0 ? (max = i) : null;
+  }
+  return max;
+}
+
+function solution(numer1, denom1, numer2, denom2) {
+  let num = numer1 * denom2 + numer2 * denom1;
+  let den = denom1 * denom2;
+  let maximum = 1;
+  let sum = fractionalAddition(num, den, maximum); // 분수 덧셈 함수
+  return [num / sum, den / sum];
+}
