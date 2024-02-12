a2338f55621c6a21531c802a5c4d80ea17ea2c81
jinah-dev <155082326+jinah-dev@users.noreply.github.com>
2024-02-13 02:59:54 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+    let answer = [];
+    for (let x of numbers){
+        answer.push(x * 2);
+    }
+    return answer;
+}
+
+
+/*
+function solution(numbers) {
+    let answer = [];
+    for(i = 0; i < numbers.length; i++){
+        answer[i] = numbers[i] * 2;
+    }
+    return answer;
+}
+*/
