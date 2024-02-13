a1a80517f478a1df1adf26bad38d5b0b8b482ced
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-02-14 03:40:35 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -3,16 +3,26 @@ function solution(numer1, denom1, numer2, denom2) {
     let numer = (numer1 * denom2) + (numer2 * denom1);
     let denom = denom1 * denom2;
     
-    const getGcd = (a, b) => {
-        while (b !== 0) {
-            let temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-        }
+//     const getGcd = (a, b) => {
+//         while (b !== 0) {
+//             let temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
+//         return a;
+//         }
     
-    let gcd = getGcd(numer, denom);
+//     let gcd = getGcd(numer, denom);
+    
+    const num = numer < denom ? numer : denom;
+    let arr = [];
+    
+    for (let i = 1; i <= num; i++) {
+        if (numer % i === 0 && denom % i === 0) {
+            arr.push(i);
+        }
+    }
+    const gcd = Math.max(...arr);
 
     answer[0] = numer / gcd;
     answer[1] = denom / gcd;
