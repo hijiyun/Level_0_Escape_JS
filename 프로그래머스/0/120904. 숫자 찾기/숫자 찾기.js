10c30e8960827e377189b52b2e93c6cb994ba65a
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-02-20 19:01:58 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,8 +1,12 @@
+// function solution(num, k) {
+//     const arr = Array.from(String(num), (i) => Number(i))
+//     if(arr.includes(k)) {
+//         return arr.indexOf(k) + 1;
+//     } else {
+//         return -1;
+//     }
+// }
+
 function solution(num, k) {
-    const arr = Array.from(String(num), (i) => Number(i))
-    if(arr.includes(k)) {
-        return arr.indexOf(k) + 1;
-    } else {
-        return -1;
-    }
+    return('A' + num).indexOf(k);
 }
