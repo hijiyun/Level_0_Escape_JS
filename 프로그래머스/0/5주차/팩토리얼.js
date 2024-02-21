9a13ee69ec1121a16acf94145d7bd7fe6240d0d5
JinahKim <kimjinah.dev@gmail.com>
2024-02-21 11:15:54 +0900

    5주차

--- 프로그래머스/0/5주차/팩토리얼.js
+++ 프로그래머스/0/5주차/팩토리얼.js
@@ -9,18 +9,3 @@ function solution(n) {
     }
   }
 }
-
-
-/*
-function solution(n) {
-  let fac = 1;
-  for (let i = 1; i < n; i++) {
-      fac *= i;
-      if (fac === n) {
-          return i;
-      } if (fac > n) {
-          return i-1;
-      }
-  }
-}
-*/
