2c1fef56184a1556efbf892296e3b8d40b566d6c
Al Tarf <142493319+Dodam0719@users.noreply.github.com>
2024-03-28 18:44:53 +0900

    [level 0] Title: k의 개수, Time: 36.89 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,6 +1,4 @@
 function solution(i, j, k) {
-    // 24.03.07 [알고리즘 한숟갈]
-    // 24.03.28 [Lv0 탈출]
     const arr = Array((j - i) + 1).fill().map((_, idx) => i + idx);
     const booleanArr = arr.map((el) => [...String(el)].map((v) => v.includes(k))).flat();
     console.log(booleanArr);
