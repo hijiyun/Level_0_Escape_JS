522d2144d12348c92a896a91227bfacc2a078829
박지윤 <tkrhkfld123@naver.com>
2024-03-31 23:36:59 +0900

    합성 수 찾기

---
+++ 11주차/합성수찾기.js
@@ -0,0 +1,31 @@
+// function solution(n) {
+//   var answer = 0;
+//   for (let i = 1; i <= n; i++) {
+//     let counter = 0;
+//     for (let j = 1; j <= i; j++) {
+//       if (i % j === 0) {
+//         counter++;
+//       }
+//     }
+//     if (counter > 2) {
+//       answer++;
+//     }
+//   }
+//   return answer;
+// }
+
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= n; i++) {
+    let counter = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        counter++;
+      }
+    }
+    if (counter > 2) {
+      answer++;
+    }
+  }
+  return answer;
+}
