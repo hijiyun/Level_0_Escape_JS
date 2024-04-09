a5558da395637ec5fab6c7c01cae7fbb434fa83e
강경미 <144299767+gaem-gaem@users.noreply.github.com>
2024-04-10 00:39:47 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    const arr= s.split(' ');
+    
+    while(arr.includes('Z')) {
+        arr.splice(arr.indexOf('Z') - 1, 2);
+    }
+    
+    return arr.reduce((a,b) => a + Number(b), 0);
+}
