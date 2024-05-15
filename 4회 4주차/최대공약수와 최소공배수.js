b91fbf2009ea82fdd4d45b54afbc7acd70b6a3a4
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-15 11:13:59 +0900

    Update 최대공약수와 최소공배수.js

--- 4회 4주차/최대공약수와 최소공배수.js
+++ 4회 4주차/최대공약수와 최소공배수.js
@@ -1,10 +1,12 @@
 function solution(n, m) {
-  const maxNumber = Math.max(
-    maxNumber <= n && maxNumber <= m && maxNumber % n && maxNumber % m
-  );
-  const minNumber = Math.min(
-    minNumber >= n && minNumber >= m && minNumber & n && minNumber & m
-  );
-  const answer = [maxNumber, minNumber];
-  return answer;
+    const gcd = (a, b) => {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    };
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+    return [gcd(n, m), lcm(n, m)];
 }
